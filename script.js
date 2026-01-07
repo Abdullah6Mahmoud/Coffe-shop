0ca6aa2ee8aff9933e8488eb424c23096e2b4603
abdullahabdullatef51 <abdullah_a06220@cic-cairo.com>
2026-01-07 10:20:15 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+// const form = document .getElementById("content-form");
+// const name = document .getElementById("name");
+// const email = document .getElementById("email");
+// const persons = document .getElementById("persons");
+// const data = document .getElementById("data");
+// const time = document .getElementById("time");
+// form .addEventListener("submit", function(e){
+//     console.log(name.value)
+// })
+
+// // Back to Top functionality
+// const backToTop = document.getElementById('backToTop');
+
+// if (backToTop) {
+//     window.addEventListener('scroll', () => {
+//         if (window.scrollY > 300) {
+//             backToTop.style.display = 'flex';
+//             backToTop.style.opacity = '1';
+//         } else {
+//             backToTop.style.opacity = '0';
+//             setTimeout(() => {
+//                 if (window.scrollY <= 300) backToTop.style.display = 'none';
+//             }, 300);
+//         }
+//     });
+
+//     backToTop.addEventListener('click', (e) => {
+//         e.preventDefault();
+//         window.scrollTo({
+//             top: 0,
+//             behavior: 'smooth'
+//         });
+//     });
+// }
